a4ebde3242b8d2bbabd0e44d2dd678467dab546e
Robert Sullivan <rsullivan@pivotal.io>
2016-04-21 09:46:20 -0600

    Removing tests
    Signed-off-by: Tom Chen <tochen@pivotal.io>

--- src/test/java/io/pivotal/gemfire/PrototypeApplicationTests.java
+++ src/test/java/io/pivotal/gemfire/PrototypeApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = PrototypeApplication.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = PrototypeApplication.class)
 public class PrototypeApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+//
+//	@Test
+//	public void contextLoads() {
+//
+//
+//	}
+//
 }
